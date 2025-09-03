4a0560451fc961ffacc4ada92c0d5409091588ef
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2025-09-03 09:56:26 +0330

    created page Transactions

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainLayout from '@layouts/MainLayout/MainLayout';
 import Dashboard from '@page/Dashboard/Dashboard';
 import Accounts from '@page/Accounts/Accounts';
-
+import Transactions from '@page/Transactions/Transactions';
 
 
 const queryClient = new QueryClient()
@@ -16,6 +16,7 @@ function App() {
           <Route path='/' element={<MainLayout/>}>
             <Route index element={<Dashboard/>}/>
             <Route path='Accounts' element={<Accounts/>}/>
+            <Route path='Transactions' element={<Transactions/>}/>
           </Route>
         </Routes>
       </BrowserRouter>
---
+++ src/page/Transactions/Transactions.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Transactions:React.FC = ()=>{
+  return (
+    <div>Transactions</div>
+  )
+}
+
+export default Transactions
