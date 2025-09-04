87bc46a16c5f31d7265f6813469795de946d295a
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2025-09-04 09:24:48 +0330

    created container grid for transfer

---
+++ src/components/template/transfer/TransferHeader/TransferHeader.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const TransferHeader:React.FC =  ()=>{
+  return (
+    <div>
+        <h1 className="title">Transfer Money</h1>
+        <p className="paraph">Send money between or to external recipients</p>
+    </div>
+  )
+}
+
+export default TransferHeader
--- src/page/Transfer/Transfer.tsx
+++ src/page/Transfer/Transfer.tsx
@@ -1,9 +1,21 @@
+import TransferHeader from '@components/template/transfer/TransferHeader/TransferHeader';
+import { Send } from 'lucide-react';
 import React from 'react';
 
 
 const Transfer:React.FC =   ()=>{
   return (
-    <div>Transfer</div>
+    <>
+    <TransferHeader/>
+    <div className="grid grid-col-1 lg:grid-cols-3 gap-6">
+        <div className="lg:col-span-2">
+            container
+        </div>
+        <div>
+            container
+        </div>
+    </div>
+    </>
   )
 }
 
