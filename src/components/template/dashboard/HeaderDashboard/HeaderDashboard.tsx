4e63499486df37045e5e1628f713bbc283becf5d
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2025-09-21 11:43:39 +0330

    fixed headerDashboard

--- src/components/template/dashboard/HeaderDashboard/HeaderDashboard.tsx
+++ src/components/template/dashboard/HeaderDashboard/HeaderDashboard.tsx
@@ -6,12 +6,12 @@ import { useTranslation } from 'react-i18next'
 const HeaderDashboard:React.FC  = ()=>{
   const {t} = useTranslation();
   return (
-    <div className='flex items-center justify-between'>
+    <div className='flex items-center justify-between flex-wrap '>
         <div>
             <h1 className="title">{t('Employee Dashboard')}</h1>
             <p className="paraph">{t("Bank employee portal - Manage customers and accounts")}</p>
         </div>
-        <div className="flex gap-3">
+        <div className="flex gap-3 mt-4 md:mt-0">
                 <CustomerSearchModel/>
                 <NewAccountModel/>
         </div>
