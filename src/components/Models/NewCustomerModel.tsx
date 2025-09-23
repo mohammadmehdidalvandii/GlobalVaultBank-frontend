097c7b1062b2580ae638653e131f8a354f96a83f
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2025-09-23 11:08:51 +0330

    created lanugue customermOdel

--- src/components/Models/NewCustomerModel.tsx
+++ src/components/Models/NewCustomerModel.tsx
@@ -3,10 +3,11 @@ import * as Dialog  from '@radix-ui/react-dialog';
 import { Plus } from 'lucide-react';
 import DatePicker  from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css'
+import { useTranslation } from 'react-i18next';
 
 
 const NewCustomerModel:React.FC = ()=>{
-
+    const {t} = useTranslation()
     const [open , setOpen] = useState<boolean>(false);
     const [dateOfBirth , setDateOfBirth] = useState<Date| null>(null)
   return (
@@ -14,7 +15,7 @@ const NewCustomerModel:React.FC = ()=>{
         <Dialog.Trigger asChild>
             <button className="btn secondary gap-2 w-[150px]">
                 <Plus className='h-4 w-4'/>
-                New Customer
+                {t('New Customer')}
             </button>
         </Dialog.Trigger>
         <Dialog.Portal>
@@ -23,33 +24,33 @@ const NewCustomerModel:React.FC = ()=>{
                 <div className="dialog_header">
                     <Dialog.Title className='dialog_title flex items-center gap-2 dark:text-white font-interBold rtl:font-danaBold'>
                         <Plus className='h-5 w-5'/>
-                        Create new Customer
+                        {t("Create new Customer")}
                     </Dialog.Title>
                 </div>
                 <form action="" className="space-y-4 mt-4">
                     <div className="grid grid-cols-2 sm:grid-cols-2 gap-4">
                         <div className="space-y-2">
-                            <label htmlFor="#" className="label_style dark:text-white">First Name</label>
+                            <label htmlFor="#" className="label_style dark:text-white">{t("First Name")}</label>
                             <input type="text" className="input_style mt-2 dark:text-white" />
                         </div>
                         <div className="space-y-2">
-                            <label htmlFor="#" className="label_style dark:text-white">Last Name</label>
+                            <label htmlFor="#" className="label_style dark:text-white">{t('Last Name')}</label>
                             <input type="text" className="input_style mt-2 dark:text-white" />
                         </div>
                         <div className="space-y-2">
-                            <label htmlFor="#" className="label_style dark:text-white">Email</label>
+                            <label htmlFor="#" className="label_style dark:text-white">{t('Email')}</label>
                             <input type="text" className="input_style mt-2 dark:text-white" />
                         </div>
                         <div className="space-y-2">
-                            <label htmlFor="#" className="label_style dark:text-white">Phone</label>
+                            <label htmlFor="#" className="label_style dark:text-white">{t('Phone')}</label>
                             <input type="text" className="input_style mt-2 dark:text-white" />
                         </div>
                         <div className="space-y-2">
-                            <label htmlFor="#" className="label_style dark:text-white">National ID</label>
+                            <label htmlFor="#" className="label_style dark:text-white">{t('National ID')}</label>
                             <input type="text" className="input_style mt-2 dark:text-white" />
                         </div>
                         <div className="space-y-2">
-                            <label htmlFor="#" className="label_style dark:text-white">DateOfBirth</label>
+                            <label htmlFor="#" className="label_style dark:text-white">{t('DateOfBirth')}</label>
                             <DatePicker
                             dateFormat='YYYY/MM/DD'
                             minDate={new Date(1940,0 ,1)}
@@ -65,25 +66,25 @@ const NewCustomerModel:React.FC = ()=>{
                             />
                         </div>
                         <div className="space-y-2">
-                            <label htmlFor="#" className="label_style dark:text-white">Street</label>
+                            <label htmlFor="#" className="label_style dark:text-white">{t('Street')}</label>
                             <input type="text" className="input_style mt-2 dark:text-white" />
                         </div>
                         <div className="space-y-2">
-                            <label htmlFor="#" className="label_style dark:text-white">city</label>
+                            <label htmlFor="#" className="label_style dark:text-white">{t('city')}</label>
                             <input type="text" className="input_style mt-2 dark:text-white" />
                         </div>
                         <div className="space-y-2">
-                            <label htmlFor="#" className="label_style dark:text-white">PostalCode</label>
+                            <label htmlFor="#" className="label_style dark:text-white">{t('PostalCode')}</label>
                             <input type="text" className="input_style mt-2 dark:text-white" />
                         </div>
                         <div className="space-y-2">
-                            <label htmlFor="#" className="label_style dark:text-white">Country</label>
+                            <label htmlFor="#" className="label_style dark:text-white">{t('Country')}</label>
                             <input type="text" className="input_style mt-2 dark:text-white" />
                         </div>
                     </div>
                     <div className="flex gap-3 pt-4">
-                        <button type='button' className="btn primary flex-1" onClick={()=>setOpen(false)}>Cancel</button>
-                        <button className="btn secondary flex-1">Create Customer</button>
+                        <button type='button' className="btn primary flex-1" onClick={()=>setOpen(false)}>{t('Cancel')}</button>
+                        <button className="btn secondary flex-1">{t('Create Customer')}</button>
                     </div>
                 </form>
             </Dialog.Content>
