5454b1e8e48387dacfd51e3934ea9ed54db84d3a
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2025-09-29 09:12:19 +0330

    add data DashboardTotal

--- src/components/template/dashboard/DashboardTotal/DashboardTotal.tsx
+++ src/components/template/dashboard/DashboardTotal/DashboardTotal.tsx
@@ -1,8 +1,42 @@
+import { getAccount } from '@services/accountServices';
+import { getCustomer } from '@services/customerService';
+import { useQuery } from '@tanstack/react-query';
+import { showError } from '@utils/Toasts';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 
 const DashboardTotal:React.FC =  ()=>{
-    const {t} = useTranslation()
+    const {t} = useTranslation();
+ const {
+
+    data: accounts = [],
+    isLoading: accountsLoading,
+    isError: accountsError,
+    error: accountsErrorMessage,
+  } = useQuery({
+    queryKey: ['accounts'],
+    queryFn: getAccount,
+    staleTime: 1000 * 60 * 5,
+  });
+
+  const {
+    data: customers = [],
+    isLoading: customersLoading,
+    isError: customersError,
+    error: customersErrorMessage,
+  } = useQuery({
+    queryKey: ['customers'],
+    queryFn: getCustomer,
+    staleTime: 1000 * 60 * 5,
+  });
+
+  if (accountsLoading || customersLoading) return <p>Loading...</p>;
+
+  
+  if (accountsError) showError(`${(accountsErrorMessage as Error).message}`);
+  if (customersError) showError(`${(customersErrorMessage as Error).message}`);
+
+
   return (
     <div className="rounded-lg  bg-card text-card-foreground shadow-sm gradient_hero text-white border-0">
         <div className="flex flex-col items-center p-6 ">
@@ -12,11 +46,11 @@ const DashboardTotal:React.FC =  ()=>{
             </div>
             <div className="flex justify-center gap-4">
                 <div className="text-center">
-                    <p className="text-2xl font-danaBold font-bold">127</p>
+                    <p className="text-2xl font-danaBold font-bold">{customers.length}</p>
                     <p className="text-base font-danaBold font-bold">{t('Active Customer')}</p>
                 </div>
                 <div className="text-center">
-                    <p className="text-2xl font-danaBold font-bold">342</p>
+                    <p className="text-2xl font-danaBold font-bold">{accounts.length}</p>
                     <p className="text-base font-danaBold font-bold">{t('Total Accounts')}</p>
                 </div>
                 <div className="text-center">
