b6fd7a269073f5a476cd90dd0d9f5657b4e31350
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2025-09-30 10:53:48 +0330

    fixed metaTag

--- src/page/Dashboard/Dashboard.tsx
+++ src/page/Dashboard/Dashboard.tsx
@@ -2,10 +2,12 @@ import HeaderDashboard from '@components/template/dashboard/HeaderDashboard/Head
 import DashboardTotal from '@components/template/dashboard/DashboardTotal/DashboardTotal'
 import React from 'react'
 import DashboardTransactions from '@components/template/dashboard/DashboardTransactions/DashboardTransactions'
+import MetaTag from '@hooks/metaTag'
 
 const Dashboard:React.FC = ()=>{
   return (
     <div className='space-y-6 '>
+      <MetaTag title='Dashboard' description='A bank management project for the dashboard department'/>
       <HeaderDashboard/>
       <DashboardTotal/>
       <DashboardTransactions/>
